380d8e08dcc1906de9054ef6d0b4378f974798e2
najeira <najeira@gmail.com>
2019-09-23 00:16:41 +0900

    refactor: history

--- lib/history.dart
+++ lib/history.dart
@@ -1,75 +1,199 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import 'main.dart';
 import 'service.dart';
 
-class HistoryPage extends StatefulWidget {
+class HistoryPage extends StatelessWidget {
+  HistoryPage({
+    Key key,
+    this.text,
+    this.history,
+  }) : super(key: key);
+
   final String text;
+
   final History history;
 
-  HistoryPage({this.text, this.history});
-
   @override
-  State<StatefulWidget> createState() {
-    return HistoryPageState();
+  Widget build(BuildContext context) {
+    return _buildFocusNode(context);
+  }
+
+  Widget _buildFocusNode(BuildContext context) {
+    return ChangeNotifierProvider<FocusNode>(
+      builder: (BuildContext context) => FocusNode(),
+      child: Consumer<FocusNode>(
+        builder: (BuildContext context, FocusNode focusNode, Widget child) {
+          return _buildTextNotifier(context, focusNode);
+        },
+      ),
+    );
+  }
+
+  Widget _buildTextNotifier(BuildContext context, FocusNode focusNode) {
+    return ChangeNotifierProvider<ValueNotifier<String>>(
+      builder: (BuildContext context) => ValueNotifier<String>(text),
+      child: Consumer<ValueNotifier<String>>(
+        builder: (BuildContext context, ValueNotifier<String> textNotifier, Widget child) {
+          return _buildScaffold(
+            context,
+            focusNode: focusNode,
+            textNotifier: textNotifier,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildScaffold(
+    BuildContext context, {
+    FocusNode focusNode,
+    ValueNotifier<String> textNotifier,
+  }) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Theme(
+          data: ThemeData(
+            brightness: Brightness.dark,
+            accentColor: Colors.white,
+          ),
+          child: _buildTextField(
+            context,
+            focusNode: focusNode,
+            textNotifier: textNotifier,
+          ),
+        ),
+      ),
+      body: _HistoryListView(
+        focusNode: focusNode,
+        textNotifier: textNotifier,
+        entries: history.entries,
+        onSelected: (String value) => Navigator.of(context)?.maybePop(value),
+      ),
+    );
+  }
+
+  Widget _buildTextField(
+    BuildContext context, {
+    FocusNode focusNode,
+    ValueNotifier<String> textNotifier,
+    TextEditingController controller,
+  }) {
+    return ChangeNotifierProvider<TextEditingController>(
+      builder: (BuildContext context) => TextEditingController(text: text),
+      child: Consumer<TextEditingController>(
+        builder: (BuildContext context, TextEditingController controller, Widget child) {
+          return _buildTextFieldInner(
+            context,
+            focusNode: focusNode,
+            textNotifier: textNotifier,
+            controller: controller,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildTextFieldInner(
+    BuildContext context, {
+    FocusNode focusNode,
+    ValueNotifier<String> textNotifier,
+    TextEditingController controller,
+  }) {
+    return TextField(
+      controller: controller,
+      focusNode: focusNode,
+      decoration: const InputDecoration(
+        hintText: "example.com",
+        hintStyle: TextStyle(
+          fontSize: kFontSize,
+        ),
+      ),
+      style: const TextStyle(
+        fontSize: kFontSize,
+      ),
+      keyboardType: TextInputType.url,
+      onChanged: (String value) => textNotifier.value = value,
+      onSubmitted: (String value) => textNotifier.value = value,
+      autofocus: false,
+      autocorrect: false,
+      cursorColor: Colors.white,
+    );
   }
 }
 
-class HistoryPageState extends State<HistoryPage> {
-  final ValueNotifier<String> _textNotifier = ValueNotifier<String>(null);
-  final List<String> entries = <String>[];
+typedef _ValueSelected<T> = void Function(T value);
 
-  final FocusNode _focusNode = FocusNode();
+class _HistoryListView extends StatefulWidget {
+  _HistoryListView({
+    Key key,
+    @required this.textNotifier,
+    @required this.entries,
+    @required this.onSelected,
+    @required this.focusNode,
+  }) : super(key: key);
 
-  TextEditingController _textEditingController;
+  final ValueNotifier<String> textNotifier;
+
+  final Iterable<String> entries;
+
+  final _ValueSelected<String> onSelected;
+
+  final FocusNode focusNode;
+
+  @override
+  _HistoryListViewState createState() => _HistoryListViewState();
+}
+
+class _HistoryListViewState extends State<_HistoryListView> {
   ScrollController _scrollController;
 
   @override
-  void initState() {
-    super.initState();
-
-    _textEditingController = TextEditingController(text: widget.text);
-    _textNotifier.value = widget.text;
-
-    _scrollController = ScrollController();
-    _scrollController.addListener(_onScrolled);
-
-    if (widget.history != null) {
-      entries.addAll(widget.history.entries);
-    }
+  void didChangeDependencies() {
+    _scrollController?.removeListener(_onScroll);
+    _scrollController = PrimaryScrollController.of(context);
+    _scrollController?.addListener(_onScroll);
+    super.didChangeDependencies();
   }
 
   @override
   void dispose() {
-    _textNotifier?.dispose();
-    _focusNode?.dispose();
-    _textEditingController?.dispose();
-    _scrollController?.dispose();
+    _scrollController?.removeListener(_onScroll);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData themeData = Theme.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Theme(
-          data: themeData.copyWith(
-            //accentColor: Colors.white,
-            primaryColor: Colors.white70,
-            hintColor: Colors.white30,
-          ),
-          child: _buildTextField(context),
-        ),
-      ),
-      body: _buildListView(context),
+    return ValueListenableBuilder<String>(
+      valueListenable: widget.textNotifier,
+      builder: (BuildContext context, String value, Widget child) {
+        Iterable<String> targets;
+        if (value == null || value.isEmpty) {
+          targets = widget.entries;
+        } else {
+          targets = widget.entries.where((String entry) {
+            return entry.contains(value);
+          });
+        }
+        return ListView.builder(
+          physics: AlwaysScrollableScrollPhysics(),
+          controller: _scrollController,
+          itemCount: targets.length,
+          itemBuilder: (BuildContext context, int index) {
+            return _buildListTile(context, targets.elementAt(index));
+          },
+        );
+      },
     );
   }
 
-  Widget _buildItem(BuildContext context, String value) {
+  Widget _buildListTile(BuildContext context, String value) {
     return InkWell(
+      key: ValueKey<String>(value),
       onTap: () {
-        _onItemPressed(value);
+        widget.onSelected?.call(value);
       },
       child: Container(
         padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
@@ -88,62 +212,9 @@ class HistoryPageState extends State<HistoryPage> {
     );
   }
 
-  Widget _buildTextField(BuildContext context) {
-    final ThemeData themeData = Theme.of(context);
-    final TextStyle inputStyle = themeData.textTheme.subhead;
-    return TextField(
-      controller: _textEditingController,
-      focusNode: _focusNode,
-      decoration: InputDecoration(
-        hintText: "example.com",
-        hintStyle: inputStyle.copyWith(
-          color: Colors.white38,
-          fontSize: kFontSize,
-        ),
-      ),
-      style: inputStyle.copyWith(
-        color: Colors.white,
-        fontSize: kFontSize,
-      ),
-      keyboardType: TextInputType.url,
-      onChanged: (String value) => _textNotifier.value = value,
-      onSubmitted: (String value) => _textNotifier.value = value,
-      autofocus: false,
-      autocorrect: false,
-      showCursor: true,
-      cursorColor: Colors.white,
-    );
-  }
-
-  Widget _buildListView(BuildContext context) {
-    return ValueListenableBuilder<String>(
-      valueListenable: _textNotifier,
-      builder: (BuildContext context, String value, Widget child) {
-        List<String> targets;
-        if (value == null || value.isEmpty) {
-          targets = entries;
-        } else {
-          targets = entries.where((String entry) {
-            return entry.contains(value);
-          }).toList();
-        }
-
-        return ListView.builder(
-          controller: _scrollController,
-          itemCount: targets.length,
-          itemBuilder: (BuildContext context, int index) {
-            return _buildItem(context, targets[index]);
-          },
-        );
-      },
-    );
-  }
-
-  void _onItemPressed(String value) {
-    Navigator.of(context)?.maybePop(value);
-  }
-
-  void _onScrolled() {
-    _focusNode?.unfocus();
+  void _onScroll() {
+    if (mounted) {
+      widget.focusNode?.unfocus();
+    }
   }
 }
