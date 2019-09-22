47fb61af9e2074f30dbc028480eda198146324f0
najeira <najeira@gmail.com>
2019-09-22 18:53:49 +0900

    format history

--- lib/history.dart
+++ lib/history.dart
@@ -5,59 +5,59 @@ import 'service.dart';
 class HistoryPage extends StatefulWidget {
   final String text;
   final History history;
-  
+
   HistoryPage({this.text, this.history});
-  
+
   @override
   State<StatefulWidget> createState() {
-    return new HistoryPageState();
+    return HistoryPageState();
   }
 }
 
 class HistoryPageState extends State<HistoryPage> {
-  FocusNode focusNode = new FocusNode();
+  FocusNode focusNode = FocusNode();
   TextEditingController textEditingController;
-  
+
   String searchText;
-  
+
   final List<String> entries = <String>[];
   List<String> targets = <String>[];
-  
+
   @override
   void initState() {
     super.initState();
-    
-    textEditingController = new TextEditingController(text: widget.text);
-    
+
+    textEditingController = TextEditingController(text: widget.text);
+
     if (widget.history != null) {
-      widget.history.entries.forEach((String value){
+      widget.history.entries.forEach((String value) {
         entries.add(value);
       });
     }
-    
+
     onSearchTextChanged(widget.text);
   }
-  
+
   @override
   Widget build(BuildContext context) {
     final String text = textEditingController.text;
     final bool hasText = text != null && text.isNotEmpty;
-    
+
     final ThemeData themeData = Theme.of(context);
     final TextStyle inputStyle = themeData.textTheme.subhead;
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Theme(
+    return Scaffold(
+      appBar: AppBar(
+        title: Theme(
           data: themeData.copyWith(
             //accentColor: Colors.white,
             primaryColor: Colors.white70,
             hintColor: Colors.white30,
           ),
-          child: new TextField(
+          child: TextField(
             controller: textEditingController,
             focusNode: focusNode,
-            decoration: new InputDecoration(
-              //icon: new Icon(Icons.search),
+            decoration: InputDecoration(
+              //icon: Icon(Icons.search),
               //hideDivider: true,
               hintText: "example.com",
               hintStyle: inputStyle.copyWith(
@@ -78,59 +78,61 @@ class HistoryPageState extends State<HistoryPage> {
           ),
         ),
       ),
-      body: new ListView(
-        children: new List.generate(targets.length, (int index) {
+      body: ListView(
+        children: List.generate(targets.length, (int index) {
           return buildItem(context, targets[index]);
         }),
       ),
     );
   }
-  
+
   Widget buildItem(BuildContext context, String value) {
-    return new InkWell(
+    return InkWell(
       onTap: () {
         onItemPressed(value);
       },
-      child: new Container(
+      child: Container(
         padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
-        decoration: new BoxDecoration(
-          border: new Border(bottom: new BorderSide(
+        decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(
             color: Colors.grey[300],
           )),
         ),
-        child: new Text(value, style: const TextStyle(
-          fontSize: 18.0,
-          //fontWeight: FontWeight.w500,
-        )),
+        child: Text(value,
+            style: const TextStyle(
+              fontSize: 18.0,
+              //fontWeight: FontWeight.w500,
+            )),
       ),
     );
   }
-  
+
   void onItemPressed(String value) {
     Navigator.of(context)?.maybePop(value);
   }
-  
+
   void onSearchTextChanged(String value) {
     searchText = value;
-    
+
     if (value == null || value.isEmpty) {
       setState(() {
         targets = entries;
       });
       return;
     }
-    
+
     var matches = entries.where((String entry) {
       if (entry.contains(value)) {
         return true;
       }
       return false;
     }).toList();
-    
+
     if (targets.length == matches.length) {
       return;
     }
-    
+
     setState(() {
       targets = matches;
     });
